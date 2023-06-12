c2db1301157e78056dbd007485672006fb5545e1
LucasFontesB <126528119+LucasFontesB@users.noreply.github.com>
2023-06-12 15:59:28 -0300

    Site das Torneiras

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".slider").bxSlider({
+        auto: true,
+        mode: "fade",
+        speed: 500,
+        slideWidth: 900,
+        controls: false,
+    });
+  });
+
